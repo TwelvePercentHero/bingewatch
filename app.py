25ab5c621a7544ef642bc07632b46314f334db52
Phil Boothman <philboothman90@gmail.com>
2019-07-20 18:08:38 +0100

    Connected to MongoDB

--- app.py
+++ app.py
@@ -5,7 +5,15 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config['MONGO_DBNAME'] = 'binge_watch'
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+mongo = PyMongo(app)
+
 @app.route('/')
 @app.route('/index')
 def home_page():
-    return render_template('index.html')
+    return render_template('index.html', recipes=mongo.db.recipes.find())
+
+if __name__ == 'main':
+    app.run(host=os.environ.get('IP', '0.0.0.0'),
+            port=int(os.environ.get('PORT', '5000')))
