507f3036b6f05a4938adca66ec84495feac9e092
Phil Boothman <philboothman90@gmail.com>
2019-07-21 09:19:28 +0100

    Added basic get_recipes route

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+# Database access config
 app.config['MONGO_DBNAME'] = 'binge_watch'
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 mongo = PyMongo(app)
 
+# Home Page display
 @app.route('/')
 @app.route('/index')
 def home_page():
-    return render_template('index.html', recipes=mongo.db.recipes.find())
+    return render_template('index.html')
 
+# Recipe read methods
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template('recipe-results.html',
+                            recipes = mongo.db.recipes.find())
+
+# Run app
 if __name__ == 'main':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
             port=int(os.environ.get('PORT', '5000')))
