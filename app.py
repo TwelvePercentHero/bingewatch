360a8c5bcdda2c273177ab040b5284366c4824c3
Phil Boothman <philboothman90@gmail.com>
2019-11-26 19:12:25 +0000

    Updated IP and PORT for Heroku deployment

--- app.py
+++ app.py
@@ -750,5 +750,5 @@ def error_message(error):
 
 # Run app
 if __name__ == 'main':
-    app.run(host=os.environ.get('IP', '0.0.0.0'),
-            port=int(os.environ.get('PORT', '5000')))
+    app.run(host=os.getenv('IP'),
+            port=os.getenv('PORT'))
